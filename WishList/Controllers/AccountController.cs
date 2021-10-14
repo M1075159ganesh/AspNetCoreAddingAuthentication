3aa960fb6e1a5954cf3e1b8735acc579f80e70a2
M1072324 <thiru@Mindtree>
2021-10-14 17:08:22 +0530

    Create The AccountController

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager ;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            this._userManager = _userManager;   
+            this._signInManager = _signInManager;
+                
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
